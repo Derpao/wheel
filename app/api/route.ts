46d58a9a6f89b1963ad98af4afdeacaee02f7052
DerpaoProNew <derpaopronew@DerpaoProNews-MacBook-Pro.local>
2025-02-09 23:56:15 +0700

    Add new feature to handle user input3

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,20 +4,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Define the interface for the data
-interface RouletteData {
-  id: number;
-  phone_number: string;
-  coupon_code: string;
-  spinsleft: number;
-  ip: string;
-  cookie: string;
-  time_stamp: string;
-}
-
 export async function GET() {
   try {
-      // เชื่อมต่อฐานข้อมูล MySQL
       const connection = await mysql.createConnection({
           host: process.env.DB1_HOST,
           user: process.env.DB1_USER,
@@ -26,7 +14,6 @@ export async function GET() {
           port: process.env.DB1_PORT ? parseInt(process.env.DB1_PORT, 10) : undefined
       });
 
-      // ดึงข้อมูล 50 แถวล่าสุด โดยเรียงตาม time_stamp
       const [rows] = await connection.execute<mysql.RowDataPacket[]>(
           "SELECT * FROM wp_wprw_roulettewheel ORDER BY time_stamp DESC LIMIT 50"
       );
--- app/api2/route.ts
+++ app/api2/route.ts
@@ -5,20 +5,8 @@ import { RowDataPacket } from "mysql2";
 
 dotenv.config();
 
-// Define the interface for the data
-interface RouletteData {
-  id: number;
-  phone_number: string;
-  coupon_code: string;
-  spinsleft: number;
-  ip: string;
-  cookie: string;
-  time_stamp: string;
-}
-
 export async function GET() {
   try {
-      // เชื่อมต่อฐานข้อมูล MySQL
       const connection = await mysql.createConnection({
           host: process.env.DB2_HOST,
           user: process.env.DB2_USER,
@@ -27,7 +15,6 @@ export async function GET() {
           port: process.env.DB2_PORT ? parseInt(process.env.DB2_PORT, 10) : undefined
       });
 
-      // ดึงข้อมูล 50 แถวล่าสุด เรียงตามเวลา
       const [rows] = await connection.execute<RowDataPacket[]>(
           "SELECT * FROM wp_wprw_roulettewheel ORDER BY time_stamp DESC LIMIT 50"
       );
